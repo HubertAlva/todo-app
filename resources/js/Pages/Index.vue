17eedc9b8baccbbf9a06639c1ed722f8f288c327
HubertAlva <110942459+HubertAlva@users.noreply.github.com>
2024-09-11 15:54:02 -0500

    set up

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+    <Head title="Todo app" />
+    <main class="bg-red-400 dark:bg-blue-500">
+        <button class="px-2 py-1 bg-white rounded-md" @click="toggleDark()">
+            Is Dark: {{ isDark }}
+        </button>
+
+        <h1 class="text-3xl text-primary">Todo app</h1>
+
+        <h2 class="text-3xl text-dark-100">Todo app</h2>
+        <h2 class="text-3xl text-dark-200">Todo app</h2>
+        <h2 class="text-3xl text-dark-300">Todo app</h2>
+        <h2 class="text-3xl text-dark-400">Todo app</h2>
+        <h2 class="text-3xl text-dark-500">Todo app</h2>
+        <h2 class="text-3xl text-dark-600">Todo app</h2>
+        <h2 class="text-3xl text-dark-700">Todo app</h2>
+
+        <h2 class="text-3xl text-light-100">Todo app</h2>
+        <h2 class="text-3xl text-light-200">Todo app</h2>
+        <h2 class="text-3xl text-light-300">Todo app</h2>
+        <h2 class="text-3xl text-light-400">Todo app</h2>
+        <h2 class="text-3xl text-light-500">Todo app</h2>
+
+        <div class="rounded-full size-7 bg-check-gradient"></div>
+    </main>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,47 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "hsl(220, 98%, 61%)",
+                light: {
+                    100: "hsl(0, 0%, 98%)",
+                    200: "hsl(236, 33%, 92%)",
+                    300: "hsl(233, 11%, 84%)",
+                    400: "hsl(236, 9%, 61%)",
+                    500: "hsl(235, 19%, 35%)",
+                },
+                dark: {
+                    100: "hsl(234, 39%, 85%)",
+                    200: "hsl(236, 33%, 92%)",
+                    300: "hsl(234, 11%, 52%)",
+                    400: "hsl(233, 14%, 35%)",
+                    500: "hsl(237, 14%, 26%)",
+                    600: "hsl(235, 24%, 19%)",
+                    700: "hsl(235, 21%, 11%)",
+                },
+            },
+            backgroundImage: {
+                "main-s-light": "url('/images/bg-mobile-light.jpg')",
+                "main-s-dark": "url('/images/bg-mobile-dark.jpg')",
+                "main-l-light": "url('/images/bg-desktop-light.jpg')",
+                "main-l-dark": "url('/images/bg-desktop-dark.jpg')",
+                "check-gradient":
+                    "linear-gradient(160deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
             },
         },
     },
